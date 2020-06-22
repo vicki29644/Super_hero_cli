6aacfa138f9c66022d0fbec83437ba808d47e836
github username <github email address>
2020-06-22 19:41:37 -0400

    vicki29644/Super_here_cli

--- lib/shc.rb
+++ lib/shc.rb
@@ -6,4 +6,4 @@ Bundler.require(:default)
 
 require_relative './super_hero_cli/cli.rb'
 require_relative './super_hero_cli/api.rb'
-require_relative './super_hero_cli/super_hero_cli.rb'
+require_relative './super_hero_cli/shc.rb'
